15a92299fe6f37bf4ebc749f8fa744fde23325b5
Tarwat <t.uddin9121@gmail.com>
2022-07-08 18:47:36 +0100

    Fix bug in formatTime
    Bug in output format

--- src/lib/formatTime.ts
+++ src/lib/formatTime.ts
@@ -1,9 +1,9 @@
 type Format = 'mm:ss' | 's';
 
 export default function formatTime(time: number, format: Format) {
-  const getSeconds = `${time % 60}.padLeft(2, '0')`;
+  const getSeconds = `${String(time % 60).padStart(2, '0')}`;
   const minutes = Math.floor(time / 60);
-  const getMinutes = `${minutes % 60}.padLeft(2, '0')`;
+  const getMinutes = `${String(minutes % 60).padStart(2, '0')}`;
 
   if (format === 'mm:ss') {
     return `${getMinutes} : ${getSeconds}`;
