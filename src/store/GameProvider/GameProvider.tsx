b98110e97ed33503cca77e25f87a24d4c086a732
Tarwat <t.uddin9121@gmail.com>
2022-07-10 18:44:17 +0100

    Refractor GameProvider
    Removed unecessary variables and methods that involved keeping track of user path and feedback received

--- src/store/GameProvider/GameProvider.tsx
+++ src/store/GameProvider/GameProvider.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useMemo } from 'react';
 
-import { Feedback, PathNode } from '../../interfaces';
-
 import GameContext from './game-context';
 
 type Props = {
@@ -11,8 +9,6 @@ type Props = {
 export default function GameProvider({ children }: Props) {
   const [isNight, setIsNight] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
-  const [path, setPath] = useState<PathNode[]>([]);
-  const [feedback, setFeedback] = useState<Feedback | null>(null);
 
   const toggleIsNight = () => {
     setIsNight((prevState) => !prevState);
@@ -22,22 +18,14 @@ export default function GameProvider({ children }: Props) {
     setIsPaused((prevState) => !prevState);
   };
 
-  const updatePath = (node: PathNode) => {
-    setPath((prevPath) => [...prevPath, node]);
-  };
-
   const gameContext = useMemo(
     () => ({
       isNight,
       isPaused,
-      path,
-      feedback,
       toggleIsNight,
       toggleIsPaused,
-      updatePath,
-      setFeedback,
     }),
-    [isNight, isPaused, feedback, path]
+    [isNight, isPaused]
   );
 
   return (
