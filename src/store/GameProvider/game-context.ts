7ec183eeba5acd655e0ea969f0c665b459405a5e
Tarwat <t.uddin9121@gmail.com>
2022-07-10 18:44:17 +0100

    Refractor GameProvider
    Removed unecessary variables and methods that involved keeping track of user path and feedback received

--- src/store/GameProvider/game-context.ts
+++ src/store/GameProvider/game-context.ts
@@ -1,16 +1,10 @@
 import { createContext } from 'react';
 
-import { Feedback, PathNode } from '../../interfaces/index';
-
 type Context = {
   isNight: boolean;
   isPaused: boolean;
-  path: PathNode[];
-  feedback: Feedback | null;
   toggleIsNight: () => void;
   toggleIsPaused: () => void;
-  updatePath: (node: PathNode) => void;
-  setFeedback: (feedback: Feedback) => void;
 };
 
 const GameContext = createContext<Context | null>(null);
